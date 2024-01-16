d892e5a53d95a89333bb3ccd7a852e80561418fd
btirthankar <tirthankar.bhattacharya89@gmail.com>
2024-01-17 02:05:57 +0530

    JavaPractice

---
+++ src/main/java/PracticeJava/JewelsAndStones.java
@@ -0,0 +1,27 @@
+package PracticeJava;
+
+import java.util.HashMap;
+// https://leetcode.com/problems/jewels-and-stones/
+public class JewelsAndStones {
+
+    public static int numJewelsInStones(String jewels, String stones) {
+        // A map to store count of each jewels in stones i.e. store occurrence of each char in stones
+        HashMap<Character, Integer> charCounts = new HashMap<>();
+        // Iterate char by char and store in map
+        for(char c : stones.toCharArray())
+            charCounts.put(c, charCounts.getOrDefault(c, 0) + 1);
+        // Variable to count jewels in stones
+        int count = 0;
+        // Iterate char by char in jewels and get the value from map
+        for(char c : jewels.toCharArray())
+            // add values for each char
+            count +=  charCounts.getOrDefault(c, 0);
+        // return count
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(numJewelsInStones("aA","aAAbbbb"));
+        System.out.println(numJewelsInStones("z","ZZ"));
+    }
+}
---
+++ src/main/java/PracticeJava/SortingTheSentence.java
@@ -0,0 +1,51 @@
+package PracticeJava;
+
+// https://leetcode.com/problems/sorting-the-sentence/
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SortingTheSentence {
+
+    // Using Array
+    public static String sortSentence(String sentence) {
+        // Splt sentence to get words
+        String[] words = sentence.split(" ");
+        // Create an array of same length as words count
+        String[] wordsWithCorrectPosition = new String[words.length];
+        // Iterate word by word
+        for(String word : words) {
+            // Extract last char from word
+            int index = Character.getNumericValue(word.charAt(word.length() - 1));
+            // Insert word at its correct position in array
+            wordsWithCorrectPosition[index - 1] = word.substring(0, word.length()-1);
+        }
+        // Join all words of array with whitespace as delimiter
+        return String.join(" ", wordsWithCorrectPosition);
+    }
+
+    // Using ArrayList
+    public static String sortSentenceWithArrayList(String sentence) {
+        // Splt sentence to get words
+        String[] words = sentence.split(" ");
+        // Create a new ArrayList from above array
+        ArrayList<String> wordsList = new ArrayList<>(Arrays.asList(words));
+        // Iterate word by word
+        for(String word : words) {
+            // Extract last char from word
+            int index = Character.getNumericValue(word.charAt(word.length() - 1));
+            // Replace word at its correct position in ArrayList
+            wordsList.set(index - 1, word.substring(0, word.length()-1));
+        }
+        // Join all words of array with whitespace as delimiter
+        return String.join(" ", wordsList);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sortSentence("is2 sentence4 This1 a3"));
+        System.out.println(sortSentence("Myself2 Me1 I4 and3"));
+        System.out.println(sortSentenceWithArrayList("is2 sentence4 This1 a3"));
+        System.out.println(sortSentenceWithArrayList("Myself2 Me1 I4 and3"));
+    }
+}
