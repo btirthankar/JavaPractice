8e6e52b45549dd21643282cbbaba0cbff838abf9
btirthankar <tirthankar.bhattacharya89@gmail.com>
2024-01-17 20:19:34 +0530

    JavaPractice

--- src/main/java/PracticeJava/BubbleSort.java
+++ src/main/java/PracticeJava/BubbleSort.java
@@ -11,9 +11,6 @@ public class BubbleSort {
     public static void bubbleSort(int[] arr) {
         for (int i = 0;i<arr.length-1;i++){
             for (int j = 0;j<arr.length-i-1;j++){
-                System.out.println(Arrays.toString(arr));
-                System.out.println("comparing between "+arr[j]+" and "+arr[j+1]);
-                //int a = 0;
                 if (arr[j]>arr[j+1]){
                     int temp = arr[j];
                     arr[j]= arr[j+1];
@@ -21,6 +18,5 @@ public class BubbleSort {
                 }
             }
         }
-        System.out.println(Arrays.toString(arr));
     }
 }
