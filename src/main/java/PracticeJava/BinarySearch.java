6c53aac0137b5ebd69fa3b58afd093ab9b3d29d2
btirthankar <tirthankar.bhattacharya89@gmail.com>
2024-01-18 00:31:04 +0530

    JavaPractice

---
+++ src/main/java/PracticeJava/BinarySearch.java
@@ -0,0 +1,23 @@
+package PracticeJava;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {1,4,6,7,12,45,34,56,75,84};
+        System.out.println(binarySearch(arr, 56));
+    }
+
+    public static int binarySearch(int[] arr, int a){
+        int low = 0;
+        int high = arr.length-1;
+        while (low<=high){
+            int mid = (low + high) /2;
+            if (arr[mid]== a)
+                return mid;
+            else if (arr[mid]<a)
+                low= mid+1;
+            else
+                high=mid-1;
+        }
+        return -1;
+    }
+}
